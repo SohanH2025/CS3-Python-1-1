e8d974991f7b81a4237e38cf6dd4f118be8daa4f
SohanH2025 <sohan.hormuzdiar2025@gbwl.org>
2024-09-20 14:43:32 +0000

    coded

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
+from trivia import *
+
 def main():
-    print("hello world")
+    answers = []
+    for num in range(len(trivia)):
+        print(trivia[num]["question"])
+        for option in trivia[num]["options"]:
+            print(option)
+        
+        answers.append(get_inp())
+        print(answers)
+        
+
+    
+        
+
+
+
+
+def get_inp():
+    ax = True
+    while (ax):
+        inp = input("a, b, c, d? \n").lower()
+        if (inp == "a" or inp == "b" or inp == "c" or inp == "d"):
+            ax = False
+            print(f"you selected {inp}")
+            return inp
+        else :
+            print("invalid answer")
+
 
 
 if __name__ == "__main__":
---
+++ trivia.py
@@ -0,0 +1,52 @@
+trivia = [
+        {
+            "question": "1",
+            "answer": "",
+            "options": ["a", "b", "c", "d"]
+        },
+        {
+            "question": "2",
+            "answer": "a",
+            "options": ["a", "b", "c", "d"]
+        },
+        {
+            "question": "",
+            "answer": "",
+            "options": ["a", "b", "c", "d"]
+        },
+        {
+            "question": "",
+            "answer": "",
+            "options": ["a", "b", "c", "d"]
+        },
+        {
+            "question": "",
+            "answer": "",
+            "options": ["a", "b", "c", "d"]
+        },
+        {
+            "question": "",
+            "answer": "",
+            "options": ["a", "b", "c", "d"]
+        },
+        {
+            "question": "",
+            "answer": "",
+            "options": ["a", "b", "c", "d"]
+        },
+        {
+            "question": "",
+            "answer": "",
+            "options": ["a", "b", "c", "d"]
+        },
+        {
+            "question": "",
+            "answer": "",
+            "options": ["a", "b", "c", "d"]
+        },
+        {
+            "question": "",
+            "answer": "",
+            "options": ["a", "b", "c", "d"]
+        }
+    ]
