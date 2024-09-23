ad6898cb6edbda512d9ae22c2388b2db32e74a04
SohanH2025 <sohan.hormuzdiar2025@gbwl.org>
2024-09-23 13:47:36 +0000

    added code

--- main.py
+++ main.py
@@ -1,27 +1,47 @@
 from trivia import *
+import random 
 
 def main():
-    answers = []
-    for num in range(len(trivia)):
-        print(trivia[num]["question"])
-        for option in trivia[num]["options"]:
-            print(option)
-        
-        answers.append(get_inp())
+    rand = random.Random()
+    answers = [] 
+    questions = []
+    score = 0
+    
+    for num in range(len(trivia)): #for how ever many questions there are, make a list of all the indexes
+        questions.append(num)
+    #print(questions)
+    
+
+    loop = range(len(trivia))
+    for num in loop:
+        ques = rand.randrange(len(questions)) #select index of a random question in questions
+        print(ques)
+        ask_question(trivia[questions[ques]])
+        answers.append(get_inp())  
         print(answers)
+
+        questions.remove(questions[ques])#remove the questions index so it doesn't get caled again 
         
+    
 
     
         
 
+def ask_question(question): #print trivia question and answers 
+    print(question["question"])
+    for option in question["options"]:
+        print(option)
+
+        
 
 
 
-def get_inp():
+
+def get_inp(): #loop until valid input 
     ax = True
     while (ax):
         inp = input("a, b, c, d? \n").lower()
-        if (inp == "a" or inp == "b" or inp == "c" or inp == "d"):
+        if (inp == "a" or inp == "b" or inp == "c" or inp == "d" or inp == "e" or inp == "f" or inp == "g" or inp == "h"):
             ax = False
             print(f"you selected {inp}")
             return inp
--- trivia.py
+++ trivia.py
@@ -1,52 +1,82 @@
 trivia = [
         {
-            "question": "1",
+            "question": "tester question #1",
             "answer": "",
-            "options": ["a", "b", "c", "d"]
+            "options": ["a", "b", "c", "d"],
+            "correct_output": "",
+            "incorrect_output": "",
+            "score": 0
         },
         {
-            "question": "2",
+            "question": "tester question #2",
             "answer": "a",
-            "options": ["a", "b", "c", "d"]
+            "options": ["a", "b", "c", "d"],
+            "correct_output": "",
+            "incorrect_output": "",
+            "score": 0
         },
         {
-            "question": "",
+            "question": "tester question #3",
             "answer": "",
-            "options": ["a", "b", "c", "d"]
+            "options": ["a", "b", "c", "d"],
+            "correct_output": "",
+            "incorrect_output": "",
+            "score": 0
         },
         {
-            "question": "",
+            "question": "tester question #4",
             "answer": "",
-            "options": ["a", "b", "c", "d"]
+            "options": ["a", "b", "c", "d"],
+            "correct_output": "",
+            "incorrect_output": "",
+            "score": 0
         },
         {
-            "question": "",
+            "question": "tester question #5",
             "answer": "",
-            "options": ["a", "b", "c", "d"]
+            "options": ["a", "b", "c", "d"],
+            "correct_output": "",
+            "incorrect_output": "",
+            "score": 0
         },
         {
-            "question": "",
+            "question": "tester question #6",
             "answer": "",
-            "options": ["a", "b", "c", "d"]
+            "options": ["a", "b", "c", "d"],
+            "correct_output": "",
+            "incorrect_output": "",
+            "score": 0
         },
         {
-            "question": "",
+            "question": "tester question #7",
             "answer": "",
-            "options": ["a", "b", "c", "d"]
+            "options": ["a", "b", "c", "d"],
+            "correct_output": "",
+            "incorrect_output": "",
+            "score": 0
         },
         {
-            "question": "",
+            "question": "tester question #8",
             "answer": "",
-            "options": ["a", "b", "c", "d"]
+            "options": ["a", "b", "c", "d"],
+            "correct_output": "",
+            "incorrect_output": "",
+            "score": 0
         },
         {
-            "question": "",
+            "question": "tester question #9",
             "answer": "",
-            "options": ["a", "b", "c", "d"]
+            "options": ["a", "b", "c", "d"],
+            "correct_output": "",
+            "incorrect_output": "",
+            "score": 0
         },
         {
-            "question": "",
+            "question": "tester question #10",
             "answer": "",
-            "options": ["a", "b", "c", "d"]
+            "options": ["a", "b", "c", "d"],
+            "correct_output": "",
+            "incorrect_output": "",
+            "score": 0
         }
     ]
